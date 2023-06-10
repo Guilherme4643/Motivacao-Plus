4fc4be567fc90351b6cb5271324fd0532eeb22fe
Guilherme4643 <120650910+Guilherme4643@users.noreply.github.com>
2023-06-10 19:57:49 -0300

    Update Sevidor.py
    Signed-off-by: Guilherme4643 <120650910+Guilherme4643@users.noreply.github.com>

--- Sevidor.py
+++ Sevidor.py
@@ -1,10 +1,46 @@
 import socket
 
+import threading
+
 # Endereço IP e porta do servidor
 
 HOST = '127.0.0.1'
 
 PORT = 8080
+
+# Função que será executada em cada thread
+
+def handle_client(conexao, endereco):
+
+    print('Conexão estabelecida com', endereco)
+
+    while True:
+
+        # Recebe dados do cliente
+
+        dados = conexao.recv(1024)
+
+        if not dados:
+
+            break
+
+        # Decodifica os dados recebidos
+
+        dados_decodificados = dados.decode()
+
+        print('Dados recebidos do cliente:', dados_decodificados)
+
+        # Envia uma resposta ao cliente
+
+        resposta = 'Olá, cliente!'
+
+        conexao.send(resposta.encode())
+
+    # Fecha a conexão
+
+    conexao.close()
+
+    print('Conexão encerrada com', endereco)
 
 # Criação do socket TCP/IP
 
@@ -20,28 +56,14 @@ servidor.listen()
 
 print('Servidor aguardando conexões...')
 
-# Aceita uma conexão
+while True:
 
-conexao, endereco = servidor.accept()
+    # Aceita uma conexão
 
-print('Conexão estabelecida com', endereco)
+    conexao, endereco = servidor.accept()
 
-# Recebe dados do cliente
+    # Cria uma nova thread para lidar com a conexão
 
-dados = conexao.recv(1024)
+    thread = threading.Thread(target=handle_client, args=(conexao, endereco))
 
-# Decodifica os dados recebidos
-
-dados_decodificados = dados.decode()
-
-print('Dados recebidos do cliente:', dados_decodificados)
-
-# Envia uma resposta ao cliente
-
-resposta = 'Olá, cliente!'
-
-conexao.send(resposta.encode())
-
-# Fecha a conexão
-
-conexao.close()
+    thread.start()
